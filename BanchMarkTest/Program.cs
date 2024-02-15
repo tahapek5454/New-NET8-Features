100e6548c2c6236591f181eead6e605d2c061b54
tahapek5454 <tahapek5454@gmail.com>
2024-02-15 21:54:51 +0300

    feat: add benchmark test

---
+++ BanchMarkTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+
+
+BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
+
+
+[MemoryDiagnoser]
+[SimpleJob(RuntimeMoniker.Net80)]
+[SimpleJob(RuntimeMoniker.Net70)]
+[SimpleJob(RuntimeMoniker.Net60)]
+public class Benchmark
+{
+    private List<int> _data;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _data = Enumerable.Range(0, 100000).ToList();
+    }
+
+
+    [Benchmark(Baseline = true)]
+    public List<int> SystemLinqWhere()
+    {
+        return _data
+            .Where(x => x > 100 && x < 999)
+            .ToList();
+    }
+}
--- NewVersions/Interception.cs
+++ NewVersions/Interception.cs
@@ -5,7 +5,7 @@ namespace NewVersions
 {
     public static class Interception
     {
-        [InterceptsLocation(filePath: "C:\\Users\\taha.pek\\net\\New-NET\\NewVersions\\Program.cs", line: 138, column: 3)]
+        // [InterceptsLocation(filePath: "C:\\Users\\taha.pek\\net\\New-NET\\NewVersions\\Program.cs", line: 138, column: 3)]
         public static void InterceptMethod1(this Example example)
         {
             Console.WriteLine("Intecept Method 1");
